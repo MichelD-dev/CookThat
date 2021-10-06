79f80c82c0448af9f9e766ee1f462bec18258f02
michelD-dev <delaunaymichel@hotmail.fr>
2021-10-06 14:04:56 +0200

    Mise à jour

--- src/components/AddRecipeForm.js
+++ src/components/AddRecipeForm.js
@@ -1,22 +1,30 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faImages } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
-import Ingredients from './Ingredients'
+// import Ingredients from './Ingredients'
 
 const AddRecipeForm = () => {
-  const [ingredients, setIngredients] = useState({})
-  const [showIngredients, setShowIngredients] = useState(false)
+  const [ingredient, setIngredient] = useState('')
+  const [quantity, setQuantity] = useState()
+  const [unit, setUnit] = useState()
+  const [ingredientsList, setIngredientsList] = useState([
+    { name: '', quantity: 0, unit: 'c.a.s' },
+  ])
 
   const handleAddIngredients = e => {
     e.preventDefault()
-    setIngredients(prevIngredients => prevIngredients + 1)
-    console.log(ingredients)
-    setShowIngredients(true)
+
+    setIngredientsList([
+      ...ingredientsList,
+      { name: '', quantity: 0, unit: 'c.a.s' },
+    ])
   }
 
   const handleDifficulty = e => {
     e.preventDefault()
   }
+
+
 
   return (
     <>
@@ -67,7 +75,56 @@ const AddRecipeForm = () => {
         </div>
         {/* <Ingredients /> */}
         {/* <Cooking steps /> */}
-        {showIngredients && <Ingredients />}
+        {/* {showIngredients && <Ingredients data={ingredientsList} />} */}
+        {ingredientsList.map((ingredient, index) => (
+          <div
+            className='ui form container segment'
+            key={`ingredient-${index}`}
+          >
+            <div className='field'>
+              <label htmlFor='ingredient'>Ingrédient</label>
+              <input
+                id='ingredient'
+                type='text'
+                name='ingredient'
+                onChange={e => setIngredient(e.target.value)}
+                value={ingredient.name}
+              />
+            </div>
+
+            <div className='two fields'>
+              <div className='field'>
+                <div className='field'>
+                  <label htmlFor='quantity'>Quantité</label>
+                  <input
+                    type='text'
+                    id='quantity'
+                    onChange={e => setQuantity(e.currentTarget.value)}
+                    value={ingredient.quantity}
+                  />
+                </div>
+              </div>
+              <div className='field'>
+                <label htmlFor='unit'>Mesure</label>
+                <select
+                  id='unit'
+                  className='ui fluid search dropdown'
+                  onChange={e => setUnit(e.currentTarget.value)}
+                  value={ingredient.unit}
+                >
+                  <option value='c.a.s'>c.a.s</option>
+                  <option value='c.a.c'>c.a.c</option>
+                  <option value='cl'>centilitres</option>
+                  <option value='g'>grammes</option>
+                  <option value='portions'>portions</option>
+                </select>
+              </div>
+            </div>
+            <button className='fluid ui red button'>Supprimer</button>
+            <br />
+          </div>
+        ))}
+
         <button className='fluid ui button' onClick={handleAddIngredients}>
           Ajouter un ingrédient
         </button>
